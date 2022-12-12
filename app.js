7332ca7059abcc19fd1f59a4db292383a3caa5db
Jake Tysome <jaketysome@Jakes-MacBook-Pro.local>
2022-12-12 14:34:26 +0000

    fix 500 handler

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-const { handle404errors } = require("./controllers/controllers.errors");
+const {
+  handle404errors,
+  handle500errors,
+} = require("./controllers/controllers.errors");
 const { getTopics } = require("./controllers/controllers.news");
 
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
+
+app.use(handle500errors);
 
 app.all("*", handle404errors);
 
--- controllers/controllers.errors.js
+++ controllers/controllers.errors.js
@@ -2,3 +2,8 @@ exports.handle404errors = (req, res, next) => {
   res.status(404).send({ msg: "path not found" });
   next();
 };
+
+exports.handle500errors = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "server error" });
+};
