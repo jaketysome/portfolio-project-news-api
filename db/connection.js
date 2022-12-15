85aeb012f61b084f14b726ab25632c857de9da28
Jake Tysome <jaketysome@Jakes-MacBook-Pro.local>
2022-12-15 11:53:24 +0000

    fixes errors in connection.js & listen.js

--- db/connection.js
+++ db/connection.js
@@ -9,13 +9,12 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-const config = {};
-
-ENV === "production"
-  ? {
-      connectionString: process.env.DATABASE_URL,
-      max: 2,
-    }
-  : {};
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        max: 2,
+      }
+    : {};
 
 module.exports = new Pool(config);
